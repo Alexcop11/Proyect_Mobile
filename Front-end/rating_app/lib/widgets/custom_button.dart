a703a76480518a7284ec88fc15d42a2655771b4e
PWMCode <20213TN152@utez.edu.mx>
2025-10-14 10:49:45 -0600

    Cambios en Estructura
    Cambios en orden estructura para aligerar el proyecto y mejorar el rendimento

---
+++ Front-end/rating_app/lib/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/materil.dart';
+
+class CustomButton extends StatelessWidget {
+    final String text;
+    final VoidCallback? onPressed;
+    final bool isLoading;
+    final Color? backgroundColor;
+    final double? width;
+    final double? height;
+
+    const CustomButton({
+        super.key,
+        required this.text,
+        this.onPressed,
+        this.isLoading = false,
+        this.width,
+        this.height,
+    });
+
+    @override
+    Widget build
+
+
+
+
+
+
+
+
+
+
+
+}
